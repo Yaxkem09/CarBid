60d385dd13c46f81b71498bec771c513a63f5239
Yaxkem09 <93301933+Yaxkem09@users.noreply.github.com>
2025-11-03 08:55:45 -0600

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
 
-const isLocal = window.location.hostname === 'localhost';
-
-// ⚙️ Configurar automáticamente según el entorno
 const api = axios.create({
-  baseURL: isLocal
-    ? 'http://localhost:5000' // 👈 sin /api aquí
-    : process.env.REACT_APP_API_URL || 'https://backend-carbid-env.eba-xcwij82h.us-east-2.elasticbeanstalk.com',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
   withCredentials: true,
 });
 
